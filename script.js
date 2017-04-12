7e0d5e99f3edc3a68a9d1f864b819df762af18a8
Sanghun Kang <sanghunkang.dev@gmail.com>
2017-04-13 00:59:58 +0900

    no message

--- script.js
+++ script.js
@@ -11,82 +11,95 @@ var lyr_tile = new ol.layer.Tile({
 	source: new ol.source.OSM(),
 });
 
-// lyr-vector1
-var vectorSource = new ol.source.Vector({
-	url: '/data/res.geojson',
+// LAYER VECTOR1
+var source_v1 = new ol.source.Vector({
+	url: 'data/res.geojson',
 	projection: 'EPSG:4326',
 	format: new ol.format.GeoJSON(),
 });
-vectorSource.addFeature(new ol.Feature(new ol.geom.Circle([5e6, 7e6], 1e6)));
+// vectorSource.addFeature(new ol.Feature(new ol.geom.Circle([5e6, 7e6], 1e6)));
 
-var image = new ol.style.Circle({
-	radius: 5,
-	fill: null,
-	stroke: new ol.style.Stroke({color: 'red', width: 1})
-});
-
-
-var styleFunction = function(feature) {
-	// debugger;
-	var styles = {
-		'Point': new ol.style.Style({
-			text: new ol.style.Text({text: feature.I.name}),
-			image: image,
-		}),
-	};
-	return styles[feature.getGeometry().getType()];
+function lyr_v1_style_text(feature){
+	style_text = new ol.style.Text({
+		text: feature.getProperties()['name'],
+		font: '20px Calibri bold,sans-serif',
+	});
+	return style_text
 };
 
-var vectorLayer = new ol.layer.Vector({
-	// source: vectorSource,
-	source: new ol.source.Vector({
-		url: 'data/res.geojson',
-		projection: 'EPSG:4326',
-		format: new ol.format.GeoJSON(),
-	}),
+function lyr_v1_style_image(feature) {
+	var style_image = new ol.style.Circle({
+		radius: 10,
+		fill: new ol.style.Fill({color: 'rgba(0, 0, 255, 0.5)'}),
+		stroke: new ol.style.Stroke({color: 'red', width: 1})
+	});
+	return style_image
+};
+
+function styleFunction_point(feature){
+	var styles = new ol.style.Style({
+		text: lyr_v1_style_text(feature),
+		image: lyr_v1_style_image(feature),
+	});
+	return styles;
+};
+
+var styleFunction = function(feature) {
+	var geom = feature.getGeometry().getType();
+	if(geom == 'Point') {
+		var styles = styleFunction_point(feature);
+	};
+	return styles;
+};
+
+var lyr_v1 = new ol.layer.Vector({
+	source: source_v1,
 	style: styleFunction,
 });
 
-// lyr-vector2
-var source = new ol.source.Vector({wrapX: false}); 
-var lyr_vector2 = new ol.layer.Vector({
-	source: source
+// LAYER VECTOR2
+var source_v2 = new ol.source.Vector({wrapX: false}); 
+var lyr_v2 = new ol.layer.Vector({
+	source: source_v2
 });
 
 // MAP
 var map = new ol.Map({
 	target: 'map',
-	layers: [lyr_tile, lyr_vector2, vectorLayer],
+	layers: [lyr_tile, lyr_v1, lyr_v2],
 	view: new ol.View({
 		center: ol.proj.transform([13.327091, 52.512181],'EPSG:4326','EPSG:3857'),
 		zoom: 13,
 	})
 });
 
-// ACTIONS
-var typeSelect = document.getElementById('type');
-var draw; // global so we can remove it later
-function addInteraction() {
-	var value = typeSelect.value;
-	if (value !== 'None') {
-		draw = new ol.interaction.Draw({
-			source: source,
-			type: typeSelect.value, /** @type {ol.geom.GeometryType} */ 
-		});
-		console.log(typeSelect.value);
-		map.addInteraction(draw);
-	}
-}
 
-/**
-* Handle change event.
-*/
-typeSelect.onchange = function() {
-	map.removeInteraction(draw);
-	addInteraction();
-};
-addInteraction();
 
 
 $(document).ready(function(){
+	// ACTIONS
+	var typeSelect = document.getElementById('type');
+	// var typeSelect = $('#type');
+	console.log($('#type'))
+	var draw; // global so we can remove it later
+	function addInteraction() {
+		var value = typeSelect.value;
+		if (value !== 'None') {
+			draw = new ol.interaction.Draw({
+				source: source_v2,
+				type: typeSelect.value, /** @type {ol.geom.GeometryType} */ 
+			});
+			console.log(typeSelect.value);
+			map.addInteraction(draw);
+		}
+	};
+
+	/**
+	* Handle change event.
+	*/
+	typeSelect.onchange = function() {
+		map.removeInteraction(draw);
+		addInteraction();
+	};
+	addInteraction();
 });
